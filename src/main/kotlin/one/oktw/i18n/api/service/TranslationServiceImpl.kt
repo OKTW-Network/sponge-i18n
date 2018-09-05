5a164cae7ed52155452afce38427f6c4b5759a56
Mmis1000 <mmis10002@gmail.com>
2018-09-05 17:04:07 +0800

    add method to get real name from itemstack

--- src/main/kotlin/one/oktw/i18n/api/service/TranslationServiceImpl.kt
+++ src/main/kotlin/one/oktw/i18n/api/service/TranslationServiceImpl.kt
@@ -3,7 +3,9 @@ package one.oktw.i18n.api.service
 import one.oktw.i18n.Main.Companion.IDENTIFIER
 import one.oktw.i18n.api.I18nImpl
 import one.oktw.i18n.translation.I18nTranslation
+import org.spongepowered.api.data.key.Keys
 import org.spongepowered.api.entity.living.player.Player
+import org.spongepowered.api.item.inventory.ItemStack
 import org.spongepowered.api.text.Text
 import org.spongepowered.api.text.TranslatableText
 import org.spongepowered.api.text.serializer.TextSerializers
@@ -17,32 +19,51 @@ import java.util.*
  * TranslationServiceImpl lang;
  * Player player
  *
- * lang.of("tooltip.hasItem", "mmis1000", lang.sub("item.sword"));
- * lang.ofLiteral("mmis1000 has a", lang.sub("item.sword"));
- * lang.sub("item.sword")
- * lang.subText(l"\\", lang.sub(item.sword"),"/");
- * lang.toLocalized(player, "tooltip.hasItem", "mmis1000", lang.sub("item.sword"));
- * lang.toLocalizedLiteral(player, "mmis1000 has a", lang.sub("item.sword"));
- * lang.toLocalizedText(player, "tooltip.hasItem", "mmis1000", lang.sub("item.sword"));
- * lang.toLocalizedLiteralText(player, "mmis1000 has a", lang.sub("item.sword"));
+ * lang.ofPlaceHolder("tooltip.hasItem", "mmis1000", lang.of("item.sword"));
+ * lang.ofLiteralPlaceHolder("mmis1000 has a", lang.of("item.sword"));
+ * lang.of("item.sword")
+ * lang.translation("item.sword")
+ * lang.toPlain(player, lang.of("tooltip.hasItem", "mmis1000", lang.of("item.sword")));
+ * lang.toLegacy(player, Text.of("mmis1000 has a", lang.of("item.sword")));
+ * lang.toSponge(player, Text.of("tooltip.hasItem", "mmis1000", lang.of("item.sword")));
  * ```
  */
 @Suppress("unused", "MemberVisibilityCanBePrivate")
 class TranslationServiceImpl(private val scope: String) : TranslationService {
+    override fun fromItem(item: ItemStack): Text {
+        return item.get(Keys.DISPLAY_NAME).orElse(null)?.let {
+            val plain = it.toPlain()
+
+            if (plain.startsWith(IDENTIFIER)) {
+                val json = plain.drop(IDENTIFIER.length)
+
+                try {
+                    TextSerializers.JSON.deserialize(json)
+                } catch (err: Throwable) {
+                    it
+                }
+            } else {
+                it
+            }
+        } ?: let {
+            Text.of(item.translation)
+        }
+    }
+
     override fun ofLiteralPlaceHolder(vararg values: Any): Text {
         return ofPlaceHolder(*values)
     }
 
     override fun ofPlaceHolder(vararg values: Any): Text {
-        return Text.of(IDENTIFIER +  TextSerializers.JSON.serialize(Text.of(*values)))
+        return Text.of(IDENTIFIER + TextSerializers.JSON.serialize(Text.of(*values)))
     }
 
     override fun ofPlaceHolder(key: String, vararg values: Any): Text {
-        return Text.of(IDENTIFIER +  TextSerializers.JSON.serialize(of(key, *values)))
+        return Text.of(IDENTIFIER + TextSerializers.JSON.serialize(of(key, *values)))
     }
 
     override fun ofPlaceHolderUnscoped(key: String, vararg values: Any): Text {
-        return Text.of(IDENTIFIER +  TextSerializers.JSON.serialize(ofUnscoped(key, *values)))
+        return Text.of(IDENTIFIER + TextSerializers.JSON.serialize(ofUnscoped(key, *values)))
     }
 
     override fun of(key: String, vararg values: Any): Text {
